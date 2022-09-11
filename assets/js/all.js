930115b8386263424a4cf072bec0de76e472f441
ufo060204 <ufo060204@gmail.com>
2022-09-11 19:50:49 +0800

    Update 2022-09-11T11:50:44.975Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,5 +2,18 @@
 
 $(function () {
   console.log('Hello Bootstrap5');
+}); // Initialize Swiper
+
+var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  pagination: {
+    el: ".swiper-pagination"
+  },
+  mousewheel: true,
+  keyboard: true
 });
 //# sourceMappingURL=all.js.map
